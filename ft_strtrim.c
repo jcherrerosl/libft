cfd38a13fde51f517953df8f963cb8ce19756bc5
Juan Carlos <121610967+jcherrerosl@users.noreply.github.com>
2023-03-14 10:18:11 +0100

    trim añadido

---
+++ ft_strtrim.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juaherre <juaherre@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 01:38:49 by juaherre          #+#    #+#             */
+/*   Updated: 2023/03/14 01:38:53 by juaherre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	size_t	len;
+	char	*trimmed_str;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end]))
+		end--;
+	len = end - start + 1;
+	trimmed_str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!trimmed_str)
+		return (NULL);
+	ft_memcpy(trimmed_str, &s1[start], len);
+	trimmed_str[len] = '\0';
+	return (trimmed_str);
+}
