26e561e4f0259875a555a4b8cdcd3295073d4309
Juan Carlos <121610967+jcherrerosl@users.noreply.github.com>
2023-03-14 13:03:26 +0100

    condicion para comprobar si s1 == "";

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -22,7 +22,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (!s1 || !set)
 		return (NULL);
 	start = 0;
-	end = ft_strlen(s1) - 1;
+	if (s1[0] == '\0')
+		end = 0;
+	else
+		end = ft_strlen(s1) - 1;
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
 	while (end > start && ft_strchr(set, s1[end]))
