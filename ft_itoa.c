ebbca0070ad04698d0c1f9dd5de937833864c16f
Juan Carlos <121610967+jcherrerosl@users.noreply.github.com>
2023-03-14 17:24:07 +0100

    corregido, pero muchas lineas!

--- ft_itoa.c
+++ ft_itoa.c
@@ -17,6 +17,8 @@ static int	int_len(int n)
 	int	i;
 
 	i = 1;
+	if (n < 0)
+		n = -n;
 	while (n > 9)
 	{
 		n /= 10;
@@ -29,24 +31,30 @@ char	*ft_itoa(int n)
 {
 	int		len;
 	char	*num;
+	int		neg;
 
-	if (n < 0)
-		len = int_len(n) + 1;
-	else
-		len = int_len(n);
-	num = (char *)malloc(sizeof(char) * len + 1);
+	len = int_len(n);
+	num = (char *)malloc(sizeof(char) * len);
 	if (!num)
 		return (NULL);
-	if (n < 0)
+	if (n == -2147483648)
+		num = ft_memcpy(num, "-2147483648", len);
+	else
 	{
-		num[0] = '-';
-		n = -n;
-	}
-	while (len > 0)
-	{
-		num[len - 1] = n % 10 + '0';
-		n /= 10;
-		len--;
+		neg = 1;
+		if (n < 0)
+		{
+			neg = -1;
+			n = -n;
+		}
+		while (len > 0)
+		{
+			num[len - 1] = n % 10 + '0';
+			n /= 10;
+			len--;
+		}
+		if (neg == -1)
+			num = ft_strjoin("-", num);
 	}
 	return (num);
 }
@@ -55,7 +63,7 @@ char	*ft_itoa(int n)
 
 int	main(void)
 {
-	printf("%s\n", ft_itoa(-3989));
-	free(ft_itoa(-3989));
+	printf("%s\n", ft_itoa(-2147483648));
+	free(ft_itoa(-2147483648));
 	return (0);
 }
