e604f2210f1779d616c2d4ac6634c073ab260581
Juan Carlos <121610967+jcherrerosl@users.noreply.github.com>
2023-05-03 12:57:21 +0200

    Corregido lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,32 +6,31 @@
 /*   By: juaherre <juaherre@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 08:55:07 by juaherre          #+#    #+#             */
-/*   Updated: 2023/03/28 20:39:30 by juaherre         ###   ########.fr       */
+/*   Updated: 2023/05/03 12:56:36 by juaherre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *list, void (*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
 	t_list	*aux;
 
-	if (!list)
+	if (!lst || !f || !del)
 		return (NULL);
-	new = (t_list *)malloc(sizeof(t_list));
-	if (!new)
-		return (NULL);
-	while (list != NULL)
+	new = NULL;
+	while (lst)
 	{
-		aux = list;
-		list = aux->next;
-		if (f)
-			f(aux->content);
-		new = aux;
-		if (del)
-			del(aux);
-		free(aux);
+		aux = ft_lstnew(lst->content);
+		if (!aux)
+		{
+			ft_lstclear(&new, del);
+			return (NULL);
+		}
+		aux->content = f(aux->content);
+		ft_lstadd_back(&new, aux);
+		lst = lst->next;
 	}
 	return (new);
 }
