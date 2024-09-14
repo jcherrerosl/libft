2285c6898ccec49b8e9d78f84676403bb5953d40
jcherrerosl <jcherrerosl@gmail.com>
2024-09-14 12:54:25 +0200

    algunas correcciones de funciones hasta aquí

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -9,7 +9,7 @@ int ft_memcmp(const void *s1, const void *s2, size_t n)
     i = 0;
     str1 = (unsigned char *)s1;
     str2 = (unsigned char *)s2;
-    while (i < n && (str1 || str2))
+    while (i < n)
     {
         if (str1[i] != str2[i])
             return(str1[i] - str2[i]);
--- ft_strncpy.c
+++ ft_strncpy.c
@@ -5,11 +5,15 @@ char    *ft_strncpy(char *dest, const char *src, size_t n)
     size_t  i;
 
     i = 0;
-    while (i < n)
+    while (i < n && src[i])
     {
         dest[i] = src[i];
         i++;
     }
-    dest[i] = '\0';
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
     return (dest);
 }
